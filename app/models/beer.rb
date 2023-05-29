fa2a81e50c4f66cc923b9ca7ba18e41133020b0f
Jarkko Hulkko <jarkko.hulkko@edu.savonia.fi>
2023-05-29 12:56:33 +0300

    Viikko 2 tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    self.ratings.each do |rating|
+      sum += rating.score
+    end
+    
+    avg = self.ratings.empty? ? 0 : sum/self.ratings.count
+    avg.to_f
+  end
 end
