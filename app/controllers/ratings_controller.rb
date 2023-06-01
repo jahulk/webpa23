57336dd062ba3047b5891aa4fdc77e78c69ef2b6
Jarkko Hulkko <jarkko.hulkko@edu.savonia.fi>
2023-06-01 13:34:00 +0300

    Viikko 3 tehtävä 6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,11 +9,12 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-
+    rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    rating.user = current_user
+    rating.save
     session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
 
-    redirect_to ratings_path
+    redirect_to current_user
   end
 
   def destroy
