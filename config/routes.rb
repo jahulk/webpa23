c690f32faaf185593a6f821a17f7dcb0d98f5c02
Jarkko Hulkko <jarkko.hulkko@edu.savonia.fi>
2023-06-01 13:11:05 +0300

    Viikko 3 tehtävä 4

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   # Defines the root path route ("/")
   # root "articles#index"
