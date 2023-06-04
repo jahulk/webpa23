a5e056520161abe76becdca4a6f4dc90913e0a24
Jarkko Hulkko <jarkko.hulkko@edu.savonia.fi>
2023-06-04 09:40:44 +0300

    Viikko 3 tehtävä 8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,11 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1040,
+                                   less_than_or_equal_to: ->(_b) { Time.now.year },
+                                   only_integer: true }
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
