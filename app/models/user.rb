8e02c3da6a534f326e3843e391c09227ba1a3f1f
Jarkko Hulkko <jarkko.hulkko@edu.savonia.fi>
2023-06-05 06:15:01 +0300

    Viikko 3 tehtävät 9-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 4 }, format: { with: /\A(?=.*[A-Z])(?=.*[0-9]).{4,}\z/,
                                                          message: "must be atleast 4 characters and include one number and one uppercase letter" }
-
+  has_many :memberships, dependent: :destroy
   has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+  has_many :beer_clubs, through: :memberships
 end
